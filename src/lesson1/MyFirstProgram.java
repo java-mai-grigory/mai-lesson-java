d4a02ca1dbc107bd2cc9e1c64cdac2f910c141fa
Григорий <@DESKTOP-5A5QD7V>
2018-02-27 18:52:54 +0300

    Создал проект

---
+++ src/lesson1/MyFirstProgram.java
@@ -0,0 +1,19 @@
+package lesson1;
+
+public class MyFirstProgram {
+	
+	static public void main(String[] args)
+	{
+		String s = "Мои первая программа";
+		int a = 5;
+		double d = 3.34562;
+		boolean b = true;
+		boolean b2 = false;
+		
+		System.out.println(s + ": " + a);
+		
+		System.out.printf("%s:  %d  вывод %f", s, a, d);
+		
+		
+	}
+}
